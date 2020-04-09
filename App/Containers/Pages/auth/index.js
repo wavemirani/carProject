c4507a4337e8038aa442ae4c29ff1b690f4a7fdc
vik-addweb <vikrant.addweb@gmail.com>
2020-04-09 13:12:05 +0530

    remove cose

--- App/Containers/Pages/auth/index.js
+++
@@ -1,7 +0,0 @@
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import ForgotPassword from './forgotPassword';
-import StartUp from './startUpScreen';
-import IntroScreen from './IntroScreen';
-import Verification from './verification'
-export { SignIn, SignUp, ForgotPassword, StartUp, IntroScreen, Verification } 
